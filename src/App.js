e325d2d8e1476f18ee49ae54a6fc7ac7ba7635ab
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-06-08 15:08:33 +0900

    Wrap entire app with Provider with store

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Navbar from './components/pages/Navbar'
 import './App.css'
+// redux
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
