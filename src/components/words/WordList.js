b0015c016d0704a77bd22af7db3d2ad13418411d
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-06-09 10:38:14 +0900

    Add a get all words functionality

--- src/components/words/WordList.js
+++ src/components/words/WordList.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class WordList extends Component {
+  state = {
+    words: null
+  }
+  componentDidMount() {
+    axios.get('/words')
+      .then(res => {
+        this.setState({
+          words: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   render() {
+    let allWords = this.state.words ? 
+      this.state.words.map((word, index) =>
+        <div key={index}>
+        <p>{word.english}</p>
+        <p>{word.japanese}</p>
+        <p>{word.sentence}</p>
+        <p>{word.translation}</p>
+        </div>
+      ) :
+      <p>Oops no content</p>
+
     return (
       <div>
-        <h1>Hello</h1>
+        {allWords} 
       </div>
     )
   }
