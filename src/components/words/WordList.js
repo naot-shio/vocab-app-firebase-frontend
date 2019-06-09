c9e015919715679d208138ea7654bbd4b06c146d
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-06-09 22:41:37 +0900

    Style word list

--- src/components/words/WordList.js
+++ src/components/words/WordList.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import AuthenticationIcon from '../auth/AuthenticationIcon'
+
+// styles
+import withStyles from '@material-ui/core/styles/withStyles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import styles from '../../styles/WordListStyles'
 
 class WordList extends Component {
   state = {
-    words: null
+    sentences: null
   }
   componentDidMount() {
-    axios.get('/words')
+    axios.get('/sentences')
       .then(res => {
         this.setState({
-          words: res.data
+          sentences: res.data
         })
+        console.log(res)
       })
       .catch(err => {
         console.log(err)
@@ -18,23 +28,40 @@ class WordList extends Component {
   }
 
   render() {
-    let allWords = this.state.words ? 
-      this.state.words.map((word, index) =>
-        <div key={index}>
-        <p>{word.english}</p>
-        <p>{word.japanese}</p>
-        <p>{word.sentence}</p>
-        <p>{word.translation}</p>
-        </div>
+    const { classes } = this.props;
+    let allSentences = this.state.sentences ? 
+      this.state.sentences.map((word, index) =>
+        <Card className={classes.Card} key={index}>
+          <CardContent>
+            <div className={classes.example}>
+              <Typography variant="body2">
+                {word.sentence} 
+              </Typography>
+             
+              <Typography variant="body1">
+                {word.translation}
+              </Typography>     
+            </div>
+                  
+          </CardContent>
+        </Card>
       ) :
       <p>Oops no content</p>
 
     return (
-      <div>
-        {allWords} 
-      </div>
+      <Grid container>
+        <Grid item sm={2} xs={1} />
+        <Grid item sm={8} xs={10}>
+         {allSentences}
+        </Grid>
+        <Grid item sm={2} xs={1}>
+          <div className={classes.auth}>
+            <AuthenticationIcon />
+          </div>
+        </Grid>
+      </Grid>
     )
   }
 }
 
-export default WordList
+export default withStyles(styles)(WordList)
---
+++ src/styles/WordListStyles.js
@@ -0,0 +1,20 @@
+const styles = {
+  Card: {
+    margin: 30,
+    width: 700,
+    marginLeft: "auto",
+    marginRight: "auto"
+  },
+  english: {
+    color: "blue",
+    paddingBottom: 10
+  },
+  japanese: {
+    textIndent: "5%"
+  },
+  example: {
+    textIndent: "7%"
+  }
+}
+
+export default styles
