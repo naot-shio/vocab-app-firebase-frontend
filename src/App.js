7d5cf82f39843136a8f3ed1a3fe259ce04f24756
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-06-10 17:27:26 +0900

    Keep users logged in until the token is expired

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
 
 // JS files
 import Navbar from './components/pages/Navbar'
@@ -9,19 +11,35 @@ import './App.css'
 // redux
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import { SET_AUTHENTICATED } from './redux/types'
+import { getUserData } from './redux/actions/userActions'
 
-function App() {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Navbar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/words" component={SentenceList} />
-          </Switch>
-      </BrowserRouter>
-    </Provider>
-  );
+const token = localStorage.FBIdToken;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    console.log('needa login')
+  } else {
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common['Authorization'] = token;
+    store.dispatch(getUserData())
+  }
+}
+
+class App extends Component {
+  render() {
+     return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/words" component={SentenceList} />
+            </Switch>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
 
 export default App;
