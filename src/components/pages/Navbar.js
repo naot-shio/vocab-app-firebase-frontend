ebd4379bf07ee74c6453665ec66ec20879b719a6
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-06-11 13:01:40 +0900

    Hide an add sentence button when a user is not logged in

--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -1,24 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import CreateSentence from '../sentences/CreateSentence'
+
+// styles
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import withStyles from '@material-ui/core/styles/withStyles'
 import styles from '../../styles/NavbarStyles'
 
+// Redux
+import { connect } from 'react-redux'
+
 function Navbar (props){
   const {classes} = props;
+  const { authenticated } = props.user
   return (
     <AppBar position="static" className={classes.AppBar}>
       <Toolbar className={classes.root}>
         <Typography variant="h6" className={classes.title}>
           <Link to="/">VocabBuilder</Link>
         </Typography>
-        <CreateSentence />
+        {authenticated && <CreateSentence />}
       </Toolbar>
     </AppBar>
   );
 }
 
-export default withStyles(styles)(Navbar)
+const mapStateToProps = state => ({
+  user: state.user
+})
+
+export default connect(mapStateToProps)(withStyles(styles)(Navbar))
