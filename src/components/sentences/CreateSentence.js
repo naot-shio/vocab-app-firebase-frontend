512255a765e73f186fb80dbe7b4d85349162881f
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-06-11 12:47:08 +0900

    Style the sentence form

--- src/components/sentences/CreateSentence.js
+++ src/components/sentences/CreateSentence.js
@@ -7,9 +7,12 @@ import TextField from '@material-ui/core/TextField'
 import Dialog from '@material-ui/core/Dialog'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogTitle from '@material-ui/core/DialogTitle'
+import Fab from '@material-ui/core/Fab'
+import Grid from '@material-ui/core/Grid'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'
+import { faPlusSquare, faPlus } from '@fortawesome/free-solid-svg-icons'
 import styles from '../../styles/CreateSentenceStyles'
+import '../../styles/CreateSentenceForm.css'
 
 
 // Redux
@@ -20,7 +23,7 @@ export class CreateSentence extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false,
+      open: true,
       sentence: '',
       translation: '',
       words: [{english: "", japanese: ""}],
@@ -41,12 +44,13 @@ export class CreateSentence extends Component {
   }
 
   handleChange(evt) {
-    if (["english", "japanese"].includes(evt.target.className)) {
+    if (evt.target.name === 'sentence' || evt.target.name === 'translation') {
+      console.log('oi')
+      this.setState({[evt.target.name]: evt.target.value})
+    } else {
       let words = [...this.state.words]
       words[evt.target.dataset.id][evt.target.className] = evt.target.value
       this.setState({ words }, () => console.log(this.state.words))
-    } else {
-       this.setState({[evt.target.name]: evt.target.value})
     }
   }
   
@@ -104,34 +108,53 @@ export class CreateSentence extends Component {
                 onChange={this.handleChange}
                 fullWidth
               />
-
-              <button onClick={this.addWord}>Add a word</button>
-
-              {this.state.words.map((val, idx) => {
-                let englishId = `english-${idx}`, japaneseId = `english-${idx}`
+              <Grid container>
+                <Grid item xs={9}>
                 
-                return (
-                  <div key={idx}>
-                    <label htmlFor={englishId}>{`English #${idx + 1}`}</label>
-                    <input
-                      type="text"
-                      name={englishId}
-                      data-id={idx}
-                      id={englishId}
-                      className="english"
-                    />
-                    <label htmlFor={japaneseId}>{`Japense #${idx + 1}`}</label>
-                    <input
-                      type="text"
-                      name={japaneseId}
-                      data-id={idx}
-                      id={japaneseId}
-                      className="japanese"
-                    />
+                
+              
+                {this.state.words.map((val, idx) => {
+                  let englishId = `english-${idx}`, japaneseId = `english-${idx}`
+                  
+                  return (
+                    <div key={idx} className={classes.englishJapaneseInputForm}>
+                        <input
+                          type="text"
+                          name={englishId}
+                          data-id={idx}
+                          id={englishId}
+                          className="english"
+                          placeholder={`English: ${idx + 1}`}
+                        />
+                    
+                        <input
+                          type="text"
+                          name={japaneseId}
+                          data-id={idx}
+                          id={japaneseId}
+                          className="japanese"
+                          placeholder={`Japanese: ${idx + 1}`}
+                        />
+                      
+                    </div>
+                  )
+                })}
+                </Grid>
 
+                <Grid item xs={3}>
+                  <div className={classes.addWord}>
+                    <Fab 
+                      color="secondary" 
+                      aria-label="Add" 
+                      onClick={this.addWord}
+                    >
+                      <FontAwesomeIcon icon={faPlus} />
+                    </Fab>
                   </div>
-                )
-              })}
+                 
+                </Grid>
+              </Grid>
+              
             
 
               <Button type="submit" variant="contained" color="primary" className={classes.submitButton}>
--- src/styles/CreateSentenceStyles.js
+++ src/styles/CreateSentenceStyles.js
@@ -1,6 +1,17 @@
 const styles = {
   button: {
     color: "white"
+  },
+  englishJapaneseInputForm: {
+    paddingRight: "none"
+  },
+  addWord: {
+    marginLeft: 25,
+    marginTop: 25,
+  },
+  submitButton: {
+    marginTop: 5,
+    textAlign: 'center'
   }
 }
 
