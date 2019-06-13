7d1af26f17d210997984d06089b5003dc4fc7ebb
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-06-13 23:20:55 +0900

    Set state with the retrived data

--- src/components/sentences/WordMeaningList.js
+++ src/components/sentences/WordMeaningList.js
@@ -10,6 +10,15 @@ import withStyles from '@material-ui/core/styles/withStyles'
 import styles from '../../styles/SentenceListStyles'
 
 export class WordMeaningList extends Component {
+  state = {
+    verb: [],
+    noun: [],
+    adverb: [],
+    adjective: [],
+    exclamation: [],
+    preposition: [],
+    conjugation: []
+  }
   handleSearch = () => {
     alert(this.props.word.english)
     let searchedWord = this.props.word.english
@@ -20,52 +29,107 @@ export class WordMeaningList extends Component {
           let dataLength;
           if (res.data[0].meaning.adverb) {
             dataLength = res.data[0].meaning.adverb.length
+            let adverbList = [];
             for (let i = 0; i < dataLength; i++) {
-              console.log(res.data[0].meaning.adverb[i].definition)
-              console.log(res.data[0].meaning.adverb[i].example)
+              adverbList.push({
+                definition: res.data[0].meaning.adverb[i].definition,
+                example: res.data[0].meaning.adverb[i].example
+              })
             }
+            this.setState({
+              adverb: adverbList
+            })
+            console.log(this.state.adverb)
           }
+
           if (res.data[0].meaning.adjective) {
             dataLength = res.data[0].meaning.adjective.length
+            let adjectiveList = [];
             for (let i = 0; i < dataLength; i++) {
-              console.log(res.data[0].meaning.adjective[i].definition)
-              console.log(res.data[0].meaning.adjective[i].example)
+              adjectiveList.push({
+                definition: res.data[0].meaning.adjective[i].definition,
+                example: res.data[0].meaning.adjective[i].example
+              })
             }
+            this.setState({
+              adjective: adjectiveList
+            })
+            console.log(this.state.adjective)
           }
+
           if (res.data[0].meaning.verb) {
             dataLength = res.data[0].meaning.verb.length
+            let verbList = [];
             for (let i = 0; i < dataLength; i++) {
-              console.log(res.data[0].meaning.verb[i].definition)
-              console.log(res.data[0].meaning.verb[i].example)
+              verbList.push({
+                definition: res.data[0].meaning.verb[i].definition,
+                example: res.data[0].meaning.verb[i].example
+              })
             }
+            this.setState({
+              verb: verbList
+            })
+            console.log(this.state.verb)
           }
+
           if (res.data[0].meaning.noun) {
-            dataLength = res.data[0].meaning.verb.length
+            dataLength = res.data[0].meaning.noun.length
+            let nounList = [];
             for (let i = 0; i < dataLength; i++) {
-              console.log(res.data[0].meaning.verb[i].definition)
-              console.log(res.data[0].meaning.verb[i].example)
+              nounList.push({
+                definition: res.data[0].meaning.noun[i].definition,
+                example: res.data[0].meaning.noun[i].example
+              })
             }
+            this.setState({
+              noun: nounList
+            })
+            console.log(this.state.noun)
           }
+
           if (res.data[0].meaning.exclamation) {
             dataLength = res.data[0].meaning.exclamation.length
+            let exclamationList = [];
             for (let i = 0; i < dataLength; i++) {
-              console.log(res.data[0].meaning.exclamation[i].definition)
-              console.log(res.data[0].meaning.exclamation[i].example)
+              exclamationList.push({
+                definition: res.data[0].meaning.exclamation[i].definition,
+                example: res.data[0].meaning.exclamation[i].example
+              })
             }
+            this.setState({
+              exclamation: exclamationList
+            })
+            console.log(this.state.exclamation)
           }
+
           if (res.data[0].meaning.preposition) {
             dataLength = res.data[0].meaning.preposition.length
+            let prepositionList = [];
             for (let i = 0; i < dataLength; i++) {
-              console.log(res.data[0].meaning.preposition[i].definition)
-              console.log(res.data[0].meaning.preposition[i].example)
+              prepositionList.push({
+                definition: res.data[0].meaning.preposition[i].definition,
+                example: res.data[0].meaning.preposition[i].example
+              })
             }
+            this.setState({
+              preposition: prepositionList
+            })
+            console.log(this.state.preposition)
           }
+
           if (res.data[0].meaning.conjugation) {
             dataLength = res.data[0].meaning.conjugation.length
+            let conjugationList = [];
             for (let i = 0; i < dataLength; i++) {
-              console.log(res.data[0].meaning.conjugation[i].definition)
-              console.log(res.data[0].meaning.conjugation[i].example)
+              conjugationList.push({
+                definition: res.data[0].meaning.conjugation[i].definition,
+                example: res.data[0].meaning.conjugation[i].example
+              })
             }
+            this.setState({
+              conjugation: conjugationList
+            })
+            console.log(this.state.conjugation)
           }
         } 
       })
