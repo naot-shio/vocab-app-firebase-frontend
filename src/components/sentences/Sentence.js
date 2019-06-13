96635e43e3830629a0b33bf324c47e91f3f68a9d
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-06-13 22:40:13 +0900

    Add an icon on words

--- src/components/sentences/Sentence.js
+++ src/components/sentences/Sentence.js
@@ -9,7 +9,7 @@ import Typography from '@material-ui/core/Typography'
 import Fab from '@material-ui/core/Fab'
 import IconButton from '@material-ui/core/IconButton'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons'
+import { faEdit, faInfoCircle, faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons'
 import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons'
 import withStyles from '@material-ui/core/styles/withStyles'
 import styles from '../../styles/SentenceListStyles'
@@ -80,7 +80,7 @@ class Sentence extends Component {
             {sentence.words.map((word, index) => 
               <div key={index} className={classes.word}>
                 <Typography className={classes.indices}>{i+1}-{index+1}</Typography>
-                <Typography className={classes.english}>{word.english}: </Typography>
+                <Typography className={classes.english}><FontAwesomeIcon icon={faInfoCircle} className={classes.infoIcon} />{word.english}: </Typography>
                 <Typography className={classes.japanese}>{word.japanese}</Typography>
               </div>  
             )}
@@ -110,7 +110,7 @@ class Sentence extends Component {
             {sentence.words.map((word, index) => 
               <div key={index} className={classes.word}>
                 <Typography className={classes.indices}>{i+1}-{index+1}</Typography>
-                <Typography className={classes.english}>{word.english}: </Typography>
+                <Typography className={classes.english}><FontAwesomeIcon icon={faInfoCircle} className={classes.infoIcon} />{word.english}: </Typography>
                 <Typography className={classes.japanese}>{word.japanese}</Typography>
               </div>  
             )}
