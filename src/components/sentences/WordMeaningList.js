77838a500576fd3af47ec1f86cd5705a4b1a8acb
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-06-14 14:36:19 +0900

    Update conditions

--- src/components/sentences/WordMeaningList.js
+++ src/components/sentences/WordMeaningList.js
@@ -25,241 +25,117 @@ export class WordMeaningList extends Component {
     fetch(`https://googledictionaryapi.eu-gb.mybluemix.net/?define=${searchedWord}&lang=en`)
       .then(res => res.json())
       .then(result => {
-        console.log(result[0])
-        // if (result.data) {
-        //   let dataLength;
-        //   if (result.data[0].meaning.adverb) {
-        //     dataLength = result.data[0].meaning.adverb.length
-        //     let adverbList = [];
-        //     for (let i = 0; i < dataLength; i++) {
-        //       adverbList.push({
-        //         definition: result.data[0].meaning.adverb[i].definition,
-        //         example: result.data[0].meaning.adverb[i].example
-        //       })
-        //     }
-        //     this.setState({
-        //       adverb: adverbList
-        //     })
-        //     console.log(this.state.adverb)
-        //   }
+        console.log(result[0].meaning)
+        if (result[0].meaning) {
+          let dataLength;
+          if (result[0].meaning.adverb) {
+            dataLength = result[0].meaning.adverb.length
+            let adverbList = [];
+            for (let i = 0; i < dataLength; i++) {
+              adverbList.push({
+                definition: result[0].meaning.adverb[i].definition,
+                example: result[0].meaning.adverb[i].example
+              })
+            }
+            this.setState({
+              adverb: adverbList
+            })
+            console.log(this.state.adverb)
+          }
 
-        //   if (result.data[0].meaning.adjective) {
-        //     dataLength = result.data[0].meaning.adjective.length
-        //     let adjectiveList = [];
-        //     for (let i = 0; i < dataLength; i++) {
-        //       adjectiveList.push({
-        //         definition: result.data[0].meaning.adjective[i].definition,
-        //         example: result.data[0].meaning.adjective[i].example
-        //       })
-        //     }
-        //     this.setState({
-        //       adjective: adjectiveList
-        //     })
-        //     console.log(this.state.adjective)
-        //   }
+          if (result[0].meaning.adjective) {
+            dataLength = result[0].meaning.adjective.length
+            let adjectiveList = [];
+            for (let i = 0; i < dataLength; i++) {
+              adjectiveList.push({
+                definition: result[0].meaning.adjective[i].definition,
+                example: result[0].meaning.adjective[i].example
+              })
+            }
+            this.setState({
+              adjective: adjectiveList
+            })
+            console.log(this.state.adjective)
+          }
 
-        //   if (result.data[0].meaning.verb) {
-        //     dataLength = result.data[0].meaning.verb.length
-        //     let verbList = [];
-        //     for (let i = 0; i < dataLength; i++) {
-        //       verbList.push({
-        //         definition: result.data[0].meaning.verb[i].definition,
-        //         example: result.data[0].meaning.verb[i].example
-        //       })
-        //     }
-        //     this.setState({
-        //       verb: verbList
-        //     })
-        //     console.log(this.state.verb)
-        //   }
+          if (result[0].meaning.verb) {
+            dataLength = result[0].meaning.verb.length
+            let verbList = [];
+            for (let i = 0; i < dataLength; i++) {
+              verbList.push({
+                definition: result[0].meaning.verb[i].definition,
+                example: result[0].meaning.verb[i].example
+              })
+            }
+            this.setState({
+              verb: verbList
+            })
+            console.log(this.state.verb)
+          }
 
-        //   if (result.data[0].meaning.noun) {
-        //     if (result.data[0].meaning.noun.definition === undefined) {
-        //       console.log('not found')
-        //       return 'Not found'
-        //     }
-        //     dataLength = result.data[0].meaning.noun.length
-        //     let nounList = [];
-        //     for (let i = 0; i < dataLength; i++) {
-        //       nounList.push({
-        //         definition: result.data[0].meaning.noun[i].definition,
-        //         example: result.data[0].meaning.noun[i].example
-        //       })
-        //     }
-        //     this.setState({
-        //       noun: nounList
-        //     })
-        //     console.log(this.state.noun)
-        //   }
+          if (result[0].meaning.noun) {
+            dataLength = result[0].meaning.noun.length
+            let nounList = [];
+            for (let i = 0; i < dataLength; i++) {
+              nounList.push({
+                definition: result[0].meaning.noun[i].definition,
+                example: result[0].meaning.noun[i].example
+              })
+            }
+            this.setState({
+              noun: nounList
+            })
+            console.log(this.state.noun)
+          }
 
-        //   if (result.data[0].meaning.exclamation) {
-        //     dataLength = result.data[0].meaning.exclamation.length
-        //     let exclamationList = [];
-        //     for (let i = 0; i < dataLength; i++) {
-        //       exclamationList.push({
-        //         definition: result.data[0].meaning.exclamation[i].definition,
-        //         example: result.data[0].meaning.exclamation[i].example
-        //       })
-        //     }
-        //     this.setState({
-        //       exclamation: exclamationList
-        //     })
-        //     console.log(this.state.exclamation)
-        //   }
+          if (result[0].meaning.exclamation) {
+            dataLength = result[0].meaning.exclamation.length
+            let exclamationList = [];
+            for (let i = 0; i < dataLength; i++) {
+              exclamationList.push({
+                definition: result[0].meaning.exclamation[i].definition,
+                example: result[0].meaning.exclamation[i].example
+              })
+            }
+            this.setState({
+              exclamation: exclamationList
+            })
+            console.log(this.state.exclamation)
+          }
 
-        //   if (result.data[0].meaning.preposition) {
-        //     dataLength = result.data[0].meaning.preposition.length
-        //     let prepositionList = [];
-        //     for (let i = 0; i < dataLength; i++) {
-        //       prepositionList.push({
-        //         definition: result.data[0].meaning.preposition[i].definition,
-        //         example: result.data[0].meaning.preposition[i].example
-        //       })
-        //     }
-        //     this.setState({
-        //       preposition: prepositionList
-        //     })
-        //     console.log(this.state.preposition)
-        //   }
+          if (result[0].meaning.preposition) {
+            dataLength = result[0].meaning.preposition.length
+            let prepositionList = [];
+            for (let i = 0; i < dataLength; i++) {
+              prepositionList.push({
+                definition: result[0].meaning.preposition[i].definition,
+                example: result[0].meaning.preposition[i].example
+              })
+            }
+            this.setState({
+              preposition: prepositionList
+            })
+            console.log(this.state.preposition)
+          }
 
-        //   if (result.data[0].meaning.conjugation) {
-        //     dataLength = result.data[0].meaning.conjugation.length
-        //     let conjugationList = [];
-        //     for (let i = 0; i < dataLength; i++) {
-        //       conjugationList.push({
-        //         definition: result.data[0].meaning.conjugation[i].definition,
-        //         example: result.data[0].meaning.conjugation[i].example
-        //       })
-        //     }
-        //     this.setState({
-        //       conjugation: conjugationList
-        //     })
-        //     console.log(this.state.conjugation)
-        //   }
-        //   console.log('hello')
-        // } 
+          if (result[0].meaning.conjugation) {
+            dataLength = result[0].meaning.conjugation.length
+            let conjugationList = [];
+            for (let i = 0; i < dataLength; i++) {
+              conjugationList.push({
+                definition: result[0].meaning.conjugation[i].definition,
+                example: result[0].meaning.conjugation[i].example
+              })
+            }
+            this.setState({
+              conjugation: conjugationList
+            })
+            console.log(this.state.conjugation)
+          }
+        }
+        console.log(this.state)
       })
-      .catch(err => console.log('not found'))
-
-
-
-
-      // })
-      // .catch(err => console.log('not found'))
-        // },
-        // if (res.data) {
-        //   let dataLength;
-        //   if (res.data[0].meaning.adverb) {
-        //     dataLength = res.data[0].meaning.adverb.length
-        //     let adverbList = [];
-        //     for (let i = 0; i < dataLength; i++) {
-        //       adverbList.push({
-        //         definition: res.data[0].meaning.adverb[i].definition,
-        //         example: res.data[0].meaning.adverb[i].example
-        //       })
-        //     }
-        //     this.setState({
-        //       adverb: adverbList
-        //     })
-        //     console.log(this.state.adverb)
-        //   }
-
-        //   if (res.data[0].meaning.adjective) {
-        //     dataLength = res.data[0].meaning.adjective.length
-        //     let adjectiveList = [];
-        //     for (let i = 0; i < dataLength; i++) {
-        //       adjectiveList.push({
-        //         definition: res.data[0].meaning.adjective[i].definition,
-        //         example: res.data[0].meaning.adjective[i].example
-        //       })
-        //     }
-        //     this.setState({
-        //       adjective: adjectiveList
-        //     })
-        //     console.log(this.state.adjective)
-        //   }
-
-        //   if (res.data[0].meaning.verb) {
-        //     dataLength = res.data[0].meaning.verb.length
-        //     let verbList = [];
-        //     for (let i = 0; i < dataLength; i++) {
-        //       verbList.push({
-        //         definition: res.data[0].meaning.verb[i].definition,
-        //         example: res.data[0].meaning.verb[i].example
-        //       })
-        //     }
-        //     this.setState({
-        //       verb: verbList
-        //     })
-        //     console.log(this.state.verb)
-        //   }
-
-        //   if (res.data[0].meaning.noun) {
-        //     if (res.data[0].meaning.noun.definition === undefined) {
-        //       console.log('not found')
-        //       return 'Not found'
-        //     }
-        //     dataLength = res.data[0].meaning.noun.length
-        //     let nounList = [];
-        //     for (let i = 0; i < dataLength; i++) {
-        //       nounList.push({
-        //         definition: res.data[0].meaning.noun[i].definition,
-        //         example: res.data[0].meaning.noun[i].example
-        //       })
-        //     }
-        //     this.setState({
-        //       noun: nounList
-        //     })
-        //     console.log(this.state.noun)
-        //   }
-
-        //   if (res.data[0].meaning.exclamation) {
-        //     dataLength = res.data[0].meaning.exclamation.length
-        //     let exclamationList = [];
-        //     for (let i = 0; i < dataLength; i++) {
-        //       exclamationList.push({
-        //         definition: res.data[0].meaning.exclamation[i].definition,
-        //         example: res.data[0].meaning.exclamation[i].example
-        //       })
-        //     }
-        //     this.setState({
-        //       exclamation: exclamationList
-        //     })
-        //     console.log(this.state.exclamation)
-        //   }
-
-        //   if (res.data[0].meaning.preposition) {
-        //     dataLength = res.data[0].meaning.preposition.length
-        //     let prepositionList = [];
-        //     for (let i = 0; i < dataLength; i++) {
-        //       prepositionList.push({
-        //         definition: res.data[0].meaning.preposition[i].definition,
-        //         example: res.data[0].meaning.preposition[i].example
-        //       })
-        //     }
-        //     this.setState({
-        //       preposition: prepositionList
-        //     })
-        //     console.log(this.state.preposition)
-        //   }
-
-        //   if (res.data[0].meaning.conjugation) {
-        //     dataLength = res.data[0].meaning.conjugation.length
-        //     let conjugationList = [];
-        //     for (let i = 0; i < dataLength; i++) {
-        //       conjugationList.push({
-        //         definition: res.data[0].meaning.conjugation[i].definition,
-        //         example: res.data[0].meaning.conjugation[i].example
-        //       })
-        //     }
-        //     this.setState({
-        //       conjugation: conjugationList
-        //     })
-        //     console.log(this.state.conjugation)
-        //   }
-        // } 
-      // })
-      // .catch(err => console.log('not found'))
+      .catch(err => console.log('hello error is here!', err))
   }
   render() {
     const { index, i, word, classes } = this.props
