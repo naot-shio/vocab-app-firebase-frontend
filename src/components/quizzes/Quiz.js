54dbb6fcae2f4e035f88c68f24ff6875ea85615c
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-07-02 11:36:17 +0900

    Disable and hide button in case there's no other questions

--- src/components/quizzes/Quiz.js
+++ src/components/quizzes/Quiz.js
@@ -1,49 +1,50 @@
 import React, { Component } from "react";
-import Button from '@material-ui/core/Button'
+import Button from "@material-ui/core/Button";
 // Redux
 import { connect } from "react-redux";
 import { getRandomSentences } from "../../redux/actions/dataActions";
 
 export class Quiz extends Component {
   state = {
-    next: false,
     quizNum: 0
-  }
+  };
   componentDidMount() {
     this.props.getRandomSentences();
   }
 
-  componentDidUpdate() {
-    if (this.state.next) {
-       this.setState({ next: false })
-       console.log(this.state.quizNum)
-    }
-    console.log(this.state.next)
-  }
-
   handleClick = () => {
-    this.setState({ 
-      next: true,
+    this.setState({
       quizNum: this.state.quizNum + 1
-   }) 
-  }
-  
+    });
+  };
+
   render() {
     const { sentences } = this.props.data;
+    const { quizNum } = this.state;
 
     let randomSentences = sentences.map((sentence, i) => (
       <div key={i}>
-        <h3 style={{ display: i === this.state.quizNum ? "block" : "none" }}>{sentence.sentence}</h3>
-        <h3 style={{ display: i === this.state.quizNum ? "block" : "none" }}>{sentence.translation}</h3>
+        <h3 style={{ display: i === quizNum ? "block" : "none" }}>
+          {sentence.sentence}
+        </h3>
+        <h3 style={{ display: i === quizNum ? "block" : "none" }}>
+          {sentence.translation}
+        </h3>
       </div>
     ));
 
     return (
-      <div style={{margin: "100px"}}>
-        <Button onClick={this.handleClick}>Click me</Button>
+      <div style={{ margin: "100px" }}>
+        <Button
+          style={{ display: quizNum > sentences.length - 2 ? "none" : "block" }}
+          onClick={this.handleClick}
+          disabled={quizNum > sentences.length - 2 ? true : false}
+        >
+          Click me
+        </Button>
         <div>{randomSentences}</div>
       </div>
-    )
+    );
   }
 }
 
