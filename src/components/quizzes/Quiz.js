939f0c1e4ae8cabaa59d67d4e19c703bd896e509
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-07-02 11:24:59 +0900

    Display sentence translation pairs one by one

--- src/components/quizzes/Quiz.js
+++ src/components/quizzes/Quiz.js
@@ -1,22 +1,49 @@
 import React, { Component } from "react";
+import Button from '@material-ui/core/Button'
 // Redux
 import { connect } from "react-redux";
 import { getRandomSentences } from "../../redux/actions/dataActions";
 
 export class Quiz extends Component {
+  state = {
+    next: false,
+    quizNum: 0
+  }
   componentDidMount() {
     this.props.getRandomSentences();
   }
+
+  componentDidUpdate() {
+    if (this.state.next) {
+       this.setState({ next: false })
+       console.log(this.state.quizNum)
+    }
+    console.log(this.state.next)
+  }
+
+  handleClick = () => {
+    this.setState({ 
+      next: true,
+      quizNum: this.state.quizNum + 1
+   }) 
+  }
+  
   render() {
     const { sentences } = this.props.data;
 
     let randomSentences = sentences.map((sentence, i) => (
       <div key={i}>
-        <h3>{sentence.sentence}</h3>
-        <h3>{sentence.translation}</h3>
+        <h3 style={{ display: i === this.state.quizNum ? "block" : "none" }}>{sentence.sentence}</h3>
+        <h3 style={{ display: i === this.state.quizNum ? "block" : "none" }}>{sentence.translation}</h3>
       </div>
     ));
-    return <div>{randomSentences}</div>;
+
+    return (
+      <div style={{margin: "100px"}}>
+        <Button onClick={this.handleClick}>Click me</Button>
+        <div>{randomSentences}</div>
+      </div>
+    )
   }
 }
 
