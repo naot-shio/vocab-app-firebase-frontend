a936cb1d7f2fa72ec78b4e2a389db434a07e046b
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-07-02 14:26:39 +0900

    Add logic to check if an input value is as same as an anwser prepared

--- src/components/questions/Question.js
+++ src/components/questions/Question.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 // Redux
 import { connect } from "react-redux";
@@ -7,8 +8,10 @@ import { getRandomSentences } from "../../redux/actions/dataActions";
 
 export class Question extends Component {
   state = {
+    answer: "",
     questionNumber: 0
   };
+
   componentDidMount() {
     let numberOfQuestions;
     if (this.props.location.state === undefined) {
@@ -26,33 +29,78 @@ export class Question extends Component {
     });
   };
 
+  handleChange = evt => {
+    this.setState({
+      answer: evt.target.value
+    });
+  };
+
+  handleCheck = () => {
+    const regex = /\W/gi;
+    let inputAnswer = this.state.answer.replace(regex, "");
+    let answer = this.props.data.sentences[
+      this.state.questionNumber
+    ].sentence.replace(regex, "");
+
+    if (inputAnswer === answer) {
+      console.log("Correct");
+    } else {
+      console.log("Wrong");
+    }
+    this.setState({
+      questionNumber: this.state.questionNumber + 1
+    });
+  };
+
   render() {
     const { sentences } = this.props.data;
     const { questionNumber } = this.state;
 
     let randomSentences = sentences.map((sentence, i) => (
       <div key={i}>
-        <h3 style={{ display: i === questionNumber ? "block" : "none" }}>
-          {sentence.sentence}
-        </h3>
-        <h3 style={{ display: i === questionNumber ? "block" : "none" }}>
-          {sentence.translation}
-        </h3>
+        <div style={{ display: i === questionNumber ? "block" : "none" }}>
+          <h3>{sentence.translation}</h3>
+          <TextField
+            id="question"
+            label="Question"
+            onChange={this.handleChange}
+            type="text"
+            margin="normal"
+            fullWidth
+          />
+          <h3>{sentence.sentence}</h3>
+
+          <div style={{ display: "block", textAlign: "right" }}>
+            <Button
+              onClick={this.handleCheck}
+              style={{
+                display:
+                  questionNumber > sentences.length - 2 ? "none" : "inline"
+              }}
+              disabled={questionNumber > sentences.length - 2 ? true : false}
+            >
+              Check
+            </Button>
+
+            <Button
+              style={{
+                display:
+                  questionNumber > sentences.length - 2 ? "none" : "inline"
+              }}
+              onClick={this.handleClick}
+              disabled={questionNumber > sentences.length - 2 ? true : false}
+            >
+              Pass
+            </Button>
+          </div>
+        </div>
       </div>
     ));
 
     return (
       <div style={{ margin: "100px" }}>
-        <Button
-          style={{
-            display: questionNumber > sentences.length - 2 ? "none" : "block"
-          }}
-          onClick={this.handleClick}
-          disabled={questionNumber > sentences.length - 2 ? true : false}
-        >
-          Click me
-        </Button>
         <div>{randomSentences}</div>
+
         {questionNumber > sentences.length - 2 && (
           <Button>
             <Link to="/sentences">Go back home</Link>
