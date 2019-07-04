9537ed75f9a6348bb1f399412b15bdc375d7d06c
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-07-04 17:17:55 +0900

    Style Result

---
+++ src/styles/questions/ResultStyles.js
@@ -0,0 +1,18 @@
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+const useStyles = makeStyles(theme => ({
+  inputAnswers: {
+    marginBottom: 10,
+  },
+  inputAnswer: {
+    color: "green",
+  },
+  message: {
+    textAlign: "center"
+  },
+  homeButton: {
+    textAlign: "right"
+  }
+}));
+
+export default useStyles 
